685fe31b07081a8bc3eafb43709968f7efabf38d
Luiz Gustavo <luizg.dev@gmail.com>
2021-07-13 10:17:38 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1005

---
+++ URI Online Judge/C/1 - Beginner/1005.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    double media = ((a * 3.5) + (b * 7.5)) / 11.0;
+    printf("MEDIA = %.5lf\n", media);
+    return 0;
+}
