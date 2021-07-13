1b11fb86c6e4e0512e632eb5f88df10b551bc8a3
Luiz Gustavo <luizg.dev@gmail.com>
2021-07-13 12:26:26 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1046

---
+++ URI Online Judge/C/1 - Beginner/1046.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a > b)
+    {
+        a = (24 - a);
+        printf("O JOGO DUROU %d HORA(S)\n", (a + b));
+    }
+    else if (a == b)
+        printf("O JOGO DUROU 24 HORA(S)\n");
+    else if (b > a)
+        printf("O JOGO DUROU %d HORA(S)\n", (b - a));
+    return 0;
+}
