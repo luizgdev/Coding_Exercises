f09b897071a600f9a20f74cbf91d0a49465da3ef
Luiz Gustavo <luizg.dev@gmail.com>
2021-07-21 16:45:25 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1079

---
+++ URI Online Judge/C/1 - Beginner/1079.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    float number[3], average[3];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < 3; j++)
+            scanf("%f", &number[j]);
+        number[0] *= 2.0;
+        number[1] *= 3.0;
+        number[2] *= 5.0;
+        average[i] = ((number[0] + number[1] + number[2]) / 10.0);
+    }
+    for (i = 0; i < n; i++)
+        printf("%.1f\n", average[i]);
+    return 0;
+}
