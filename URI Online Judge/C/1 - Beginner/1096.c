0d636e24964385a753a4acf14008caff18451364
Luiz Gustavo <luizg.dev@gmail.com>
2021-07-22 10:58:19 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1096

---
+++ URI Online Judge/C/1 - Beginner/1096.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 9; i += 2)
+    {
+        for (j = 7; j >= 5; j--)
+        {
+            if (j < 5)
+                j = 7;
+            printf("I=%d J=%d\n", i, j);
+        }
+    }
+    return 0;
+}
