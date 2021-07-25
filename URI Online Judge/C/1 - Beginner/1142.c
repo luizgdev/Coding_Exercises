ba3a2fe39da556a9679456e745719b25537579e6
Luiz Gustavo <luizg.dev@gmail.com>
2021-07-25 14:33:41 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1142

---
+++ URI Online Judge/C/1 - Beginner/1142.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j = 1;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d %d %d PUM\n", j, (j + 1), (j + 2));
+        j += 4;
+    }
+    return 0;
+}
