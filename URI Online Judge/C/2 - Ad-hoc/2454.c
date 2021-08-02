5aa720df19a802b062e381446ac27f1a1fde88b0
Luiz Gustavo <luizg.dev@gmail.com>
2021-08-02 12:08:05 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/2454

---
+++ URI Online Judge/C/2 - Ad-hoc/2454.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int p, r;
+    scanf("%d %d", &p, &r);
+    if (p == 1 && r == 1)
+        printf("A\n");
+    else if (p == 1 && r == 0)
+        printf("B\n");
+    else
+        printf("C\n");
+    return 0;
+}
