e2c503ac4827dee64e4f3d2f62667fb5e3a69cc7
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-08-12 18:38:41 -0300

    https://www.urionlinejudge.com.br/judge/en/problems/view/1180

--- URI Online Judge/C/1 - Beginner/1180.c
+++ URI Online Judge/C/1 - Beginner/1180.c
@@ -5,9 +5,7 @@ int main()
     scanf("%d", &n);
     int x[n];
     for (i = 0; i < n; i++)
-    {
         scanf("%d", &x[i]);
-    }
     int menor = x[n - 1];
     for (i = 0; i < n; i++)
         if (x[i] < menor)
