1f9b324887621c4f42f14225866be4b3c295bb01
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2022-05-23 13:01:42 -0300

    https://www.beecrowd.com.br/judge/en/problems/view/1014

---
+++ URI Online Judge/C++/1 - Beginner/1014.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    float y;
+
+    cin >> x >> y;
+
+    cout << fixed << setprecision(3) << x / y << " km/l" << endl;
+
+    return 0;
+}
